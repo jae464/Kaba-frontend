fb7552261c9e481ddbb12e6587aafe9957173f96
이민재 <jae464@naver.com>
2024-07-22 16:54:06 +0900

    [FIX] 모바일 image assets png -> PNG 수정

--- src/constants/tutorials.ts
+++ src/constants/tutorials.ts
@@ -115,19 +115,19 @@ export const tutorials_mobile: Tutorial[] = [
     title: '인물관계도',
     items: [
       {
-        img_url: '/src/assets/images/인물관계도_모바일_1.png',
+        img_url: '/src/assets/images/인물관계도_모바일_1.PNG',
         description: `등장인물 관계가 너무 복잡해서 이해하기 어려우신적 있으신가요? 그럴때는 "인물관계도" 기능을 사용해봐요!`,
       },
       {
-        img_url: '/src/assets/images/인물관계도_모바일_2.png',
+        img_url: '/src/assets/images/인물관계도_모바일_2.PNG',
         description: `먼저 오른쪽 메뉴에서 인물 관계도 버튼을 클릭해요.`,
       },
       {
-        img_url: '/src/assets/images/인물관계도_모바일_3.png',
+        img_url: '/src/assets/images/인물관계도_모바일_3.PNG',
         description: 'KABA가 인물관계도를 그려줄때까지 잠시 기다려요.',
       },
       {
-        img_url: '/src/assets/images/인물관계도_모바일_4.png',
+        img_url: '/src/assets/images/인물관계도_모바일_4.PNG',
         description:
           '인물관계도가 완성되었어요! 이제 한눈에 읽은곳까지의 인물관계도를 파악해봐요.',
       },
@@ -137,12 +137,12 @@ export const tutorials_mobile: Tutorial[] = [
     title: '등장인물채팅',
     items: [
       {
-        img_url: '/src/assets/images/등장인물채팅_모바일_1.png',
+        img_url: '/src/assets/images/등장인물채팅_모바일_1.PNG',
         description:
           '소설속 등장인물과 대화해보고 싶으신 경험 있으신가요? 그럴때는 "등장인물 채팅" 기능을 이용해보세요!',
       },
       {
-        img_url: '/src/assets/images/등장인물채팅_모바일_2.png',
+        img_url: '/src/assets/images/등장인물채팅_모바일_2.PNG',
         description: '원하는 등장인물을 선택하고 마음껏 대화해보세요.',
       },
     ],
@@ -151,20 +151,20 @@ export const tutorials_mobile: Tutorial[] = [
     title: '지난줄거리',
     items: [
       {
-        img_url: '/src/assets/images/지난줄거리_모바일_1.png',
+        img_url: '/src/assets/images/지난줄거리_모바일_1.PNG',
         description:
           '혹시 읽은곳까지 내용이 기억나지 않으신적 있으신가요? 그럴때는 "지난줄거리" 요약 기능을 이용해보세요!',
       },
       {
-        img_url: '/src/assets/images/지난줄거리_모바일_2.png',
+        img_url: '/src/assets/images/지난줄거리_모바일_2.PNG',
         description: 'KABA가 읽은곳까지의 줄거리를 그림과 함께 요약해줄거에요.',
       },
       {
-        img_url: '/src/assets/images/지난줄거리_모바일_3.png',
+        img_url: '/src/assets/images/지난줄거리_모바일_3.PNG',
         description: '폴라로이드 느낌으로 4컷의 그림과 줄거리가 제공돼요.',
       },
       {
-        img_url: '/src/assets/images/지난줄거리_모바일_4.png',
+        img_url: '/src/assets/images/지난줄거리_모바일_4.PNG',
         description: '더 이상 앞에 내용이 기억안난다고 슬퍼하지 마세요!',
       },
     ],
@@ -173,25 +173,25 @@ export const tutorials_mobile: Tutorial[] = [
     title: '그림일기',
     items: [
       {
-        img_url: '/src/assets/images/그림일기_모바일_1.png',
+        img_url: '/src/assets/images/그림일기_모바일_1.PNG',
         description:
           '혹시 책을 읽다가 마음에 드는 문구를 발견했나요? 그럴땐 "그림일기" 기능을 이용해보세요!',
       },
       {
-        img_url: '/src/assets/images/그림일기_모바일_2.png',
+        img_url: '/src/assets/images/그림일기_모바일_2.PNG',
         description: 'KABA가 문구와 어울리는 그림을 만들어줄거에요.',
       },
       {
-        img_url: '/src/assets/images/그림일기_모바일_3.png',
+        img_url: '/src/assets/images/그림일기_모바일_3.PNG',
         description: '원하는 글꼴, 폰트 크기, 폰트 색상도 설정할 수 있어요.',
       },
       {
-        img_url: '/src/assets/images/그림일기_모바일_4.png',
+        img_url: '/src/assets/images/그림일기_모바일_4.PNG',
         description:
           '혹시 그림이 마음에 안들면 그림체를 바꿔서 다시 생성해봐요.',
       },
       {
-        img_url: '/src/assets/images/그림일기_모바일_5.png',
+        img_url: '/src/assets/images/그림일기_모바일_5.PNG',
         description: '그림이 마음에 들면 저장해서 친구와 공유할수 있어요.',
       },
     ],
@@ -200,12 +200,12 @@ export const tutorials_mobile: Tutorial[] = [
     title: 'KABA위키',
     items: [
       {
-        img_url: '/src/assets/images/카바위키_모바일_1.png',
+        img_url: '/src/assets/images/카바위키_모바일_1.PNG',
         description:
           '혹시 책을 읽으면서 생소한 단어를 발견하셨나요? 그럴땐 "KABA 위키" 기능을 이용해보세요!',
       },
       {
-        img_url: '/src/assets/images/카바위키_모바일_2.png',
+        img_url: '/src/assets/images/카바위키_모바일_2.PNG',
         description: 'KABA가 책의 내용을 바탕으로 자세하기 설명해줄거에요.',
       },
     ],
